9cb978e1b3ad52221693e5b62b71c26615d6df16
dyslab <45236395+dyslab@users.noreply.github.com>
2019-03-24 08:57:10 +0800

    Framework Constructing... #7 :mushroom:

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-const menu = require('./jsons/topmenu.json')
+const topmenu = require('./jsons/topmenu.json')
 const footer = require('./jsons/footer.json')
 const content = require('./jsons/index.json')
 
 // Top menu part
 var template = require(`./views/topmenu.pug`);
-document.write(template({menu: menu}));
+document.write(template({data: topmenu}));
 
 // Content part
-for (index in content) {
+for (var index in content) {
     template = require(`./views/${content[index].widget}.pug`);
     document.write(template({data: content[index].data}));
 }
 
 // Footer part
 template = require(`./views/footer.pug`);
-document.write(template({footer: footer}));
+document.write(template({data: footer}));
