e1bbfe224fbd8debac918a9f9708d43ab9ee2185
dyslab <45236395+dyslab@users.noreply.github.com>
2019-05-03 03:54:27 +0800

    Add news function :rocket:

--- webpack.build.files.index.js
+++ webpack.build.files.index.js
@@ -1,6 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-// List the html files need to be generated.
+// The List of html files that need to be generated.
 const htmlfiles = {
     index: [
         {
@@ -23,38 +23,22 @@ const htmlfiles = {
         }
     ],
     news_list: [
-        {
-            entrypoint_id: 'news_list_1',
-            template: './src/views/template_news_list.pug',
-            listjsonfile: './src/jsons/news/news_list_part2.json',   // indicate the json file where the news list saved in.
-            output: 'news_list_1.html',
-            title: '企业新闻 - 第1页'
-        },
-        {
-            entrypoint_id: 'news_list_2',
-            template: './src/views/template_news_list.pug',
-            listjsonfile: './src/jsons/news/news_list_part2.json',   // indicate the json file where the news list saved in.
-            output: 'news_list_2.html',
-            title: '企业新闻 - 第2页'
-        },
-        {
-            entrypoint_id: 'news_list_3',
-            template: './src/views/template_news_list.pug',
-            listjsonfile: './src/jsons/news/news_list_part2.json',   // indicate the json file where the news list saved in.
-            output: 'news_list_3.html',
-            title: '企业新闻 - 第3页'
-        }
+        'news_list_1',
+        'news_list_2',
+        'news_list_3'
     ],
     news: [
-        // output news file pattern: 'news_[id].html',
-        "news_20190401",
-        "news_20190400"
+        // output news document file pattern: 'news_[id].html',
+        "news_20190500", // the 1st item correspond to the template file 'new_doc_1.js',
+        "news_20190401", // the 2nd item correspond to the template file 'new_doc_2.js',
+        "news_20190400" // the 3rd item correspond to the template file 'new_doc_3.js', etc.
     ]
 }
 
 module.exports = {
     Entrypoints: function() {
         var epString = '';
+        var fileid = 0;
 
         // ep for htmlfiles.index
         for (var id in htmlfiles.index) {
@@ -62,10 +46,14 @@ module.exports = {
         }
         // ep for htmlfiles.news_list
         for (id in htmlfiles.news_list) {
-            epString += `"${htmlfiles.news_list[id].entrypoint_id}":"./src/${htmlfiles.news_list[id].entrypoint_id}.js",`;
+            fileid = parseInt(id) + 1;
+            epString += `"news_list_${fileid}":"./src/news/news_list_${fileid}.js",`;
         }
         // ep for htmlfiles.news
-        // do something here...
+        for (id in htmlfiles.news) {
+            fileid = parseInt(id) + 1;
+            epString += `"${htmlfiles.news[id]}":"./src/news/news_doc_${fileid}.js",`;
+        }
 
         if (epString !== '') {
             epString = epString.slice(0, epString.length-1)
@@ -76,6 +64,7 @@ module.exports = {
     Plugins: function() {
         var tempObj;
         var returnList = [];
+        var fileid = 0;
 
         // HtmlWebpackPlugin for htmlfiles.index
         for (var id in htmlfiles.index) {
@@ -94,21 +83,38 @@ module.exports = {
         }
         // HtmlWebpackPlugin for htmlfiles.news_list
         for (id in htmlfiles.news_list) {
+            fileid = parseInt(id) + 1;
             tempObj = new HtmlWebpackPlugin({
                 chunks: [
-                  `${htmlfiles.news_list[id].entrypoint_id}`,
+                  `news_list_${fileid}`,
                 ],
                 favicon: './src/favicon.ico',
-                filename: htmlfiles.news_list[id].output,
+                filename: `news_list_${fileid}.html`,
                 inject: 'body',
                 minify: true,
-                template: htmlfiles.news_list[id].template,
-                title: htmlfiles.news_list[id].title
+                template: './src/views/template_news_list.pug',
+                title: `企业新闻 - 列表 - 第${fileid}页`
             });
             returnList.push(tempObj);
         }
         // HtmlWebpackPlugin for htmlfiles.news
-        // do something here...
+        let content = '';
+        for (id in htmlfiles.news) {
+            fileid = parseInt(id) + 1;
+            content = require(`./src/jsons/news/${htmlfiles.news[id]}.json`);
+            tempObj = new HtmlWebpackPlugin({
+                chunks: [
+                  htmlfiles.news[id],
+                ],
+                favicon: './src/favicon.ico',
+                filename: `${htmlfiles.news[id]}.html`,
+                inject: 'body',
+                minify: true,
+                template: './src/views/template_news_doc.pug',
+                title: `企业新闻 - ${content.title}`
+            });
+            returnList.push(tempObj);
+        }
 
         return returnList
     }
