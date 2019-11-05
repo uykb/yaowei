8f11ff5cf50fa36b7e2502369118b4ec4acea767
Vincent To <45236395+dyslab@users.noreply.github.com>
2019-11-05 16:03:33 +0800

    News update #191105 🔥

---
+++ src/news/news_20191101.js
@@ -0,0 +1,24 @@
+const topmenu = require('../jsons/topmenu.json')
+const footer = require('../jsons/footer.json')
+
+// Indicate the current page no.
+// The only one parameter which need to be changed in the files 'news_list_nn.js".
+const PAGE_ID = 'news_20191101';
+let content = require(`../jsons/news/${PAGE_ID}.json`);
+
+// Top menu part
+let template = require(`../views/topmenu.pug`);
+document.write(template({data: topmenu}));
+
+// Content part
+// Part1
+let part = require('../jsons/news/news_doc_title.json');
+template = require(`../views/${part.widget}.pug`);
+document.write(template({data: part.data}));
+// Part2, The news list part.
+template = require(`../views/${content.widget}.pug`);
+document.write(template({data: content}));
+
+// Footer part
+template = require(`../views/footer.pug`);
+document.write(template({data: footer}));
